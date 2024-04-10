6bb778118bfe4a9862be52b9f1235b230deac391
Xinqi Mu <hi@muxinqi.com>
2024-04-10 21:58:51 +0800

    Add global prefix /api

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('/api');
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder().setTitle('Nodecho API').build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: true,
+  });
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(3000);
 }
